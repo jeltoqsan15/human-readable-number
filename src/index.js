de162bae68d8945459322aea6efeae82e1b5f8dc
jeltoqsan15 <realrss@mail.ru>
2023-01-08 16:16:15 +0500

    feat: add dependencies and code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+
+    const sourceNumber = number.toString();
+    
+    const zeroPosition = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const firstPositionWithOne = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const firstPosition = [null, null, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    const printOneDigitNumber = (num) => {
+        return zeroPosition[+num];
+    }
+
+    const printTwoDigitNumber = (num) => {
+        if (num[0] === '1') {
+            return firstPositionWithOne[+num[1]];
+        } else {
+            if (num[1] === '0') return firstPosition[+num[0]];
+
+            return `${firstPosition[+num[0]]} ${zeroPosition[+num[1]]}`
+        }
+    }
+
+    if (sourceNumber.length === 1) {
+        return printOneDigitNumber(sourceNumber);
+
+    } else if (sourceNumber.length === 2) {
+        return printTwoDigitNumber(sourceNumber);
+    } else {
+        if (sourceNumber[1] === '0' && sourceNumber[2] === '0') {
+            return `${printOneDigitNumber(sourceNumber[0])} hundred`;
+        } else if (sourceNumber[1] === '0') {
+            return `${printOneDigitNumber(sourceNumber[0])} hundred ${printOneDigitNumber(sourceNumber[2])}`;
+        } else {
+            return `${printOneDigitNumber(sourceNumber[0])} hundred ${printTwoDigitNumber(sourceNumber.slice(1))}`;
+        }
+    }
 }
